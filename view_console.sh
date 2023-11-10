8783754284b7a386ce4afbf4260e75cc581d98b7
Saswat Priyabrat <46641635+spriyabrat@users.noreply.github.com>
2023-11-10 10:54:43 +0530

    Update view_console.sh
    updated

--- view_console.sh
+++ view_console.sh
@@ -5,7 +5,7 @@ TAG=$1
 PARAMETERS=$2
 
 # Run the Ansible playbook with the extracted tag and parameters
-ansible-playbook -i inventory.yml view_console.yml --tags "$TAG" -e "$PARAMETERS"
+ansible-playbook2 ansible2/adhoc_playbooks/view_console.yml --tags "$TAG" -e "$PARAMETERS"
 
 # Display the contents of the text files
-./display.sh
+./ansible2/adhoc_playbooks/display_dashboard.sh
